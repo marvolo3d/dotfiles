5c2725d9526aa5e1b0cc2b66d513ca8ada35a5e2
marvolo3d <tomnorman.cg@gmail.com>
2018-01-08 23:08:55 -0800

    remove redundant append to pythonpath and add global python 2.7 site-packages to pythonpath

--- maya/userSetup.py
+++ maya/userSetup.py
@@ -2,21 +2,20 @@ import os
 import sys
 import pymel.core as pm
 
-# sublime commandport
-try: 
+#  tomcommandport
+try:
     pm.commandPort(name=":7005", sourceType="python")
     print 'commandport to atom open...'
 except:
     print 'could not open port to atom'
 
 if not pm.about(batch=1): #gui mode only
-    # append python path
-    marvolo_maya_path = '{0}/maya/scripts'.format(os.environ['DUNGEON'])
-    sys.path.append(marvolo_maya_path)
-
     # import some python modules
     # pm.evalDeferred("import zhcg_polytools")
     pm.evalDeferred("import marvolo_maya")
     pm.evalDeferred("import cosmos")
 
-    print "marvolo userSetup finish"
+# append path where GitPython lives
+sys.path.append("/usr/lib/python2.7/site-packages/")
+
+print "marvolo userSetup finish"
