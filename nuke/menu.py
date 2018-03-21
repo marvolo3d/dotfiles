588068599a73e361da1c564c93b4f7796d873bf0
marvolo3d <tomnorman.cg@gmail.com>
2018-03-20 20:30:54 -0700

    removed nuke config and add yaourtrc

--- nuke/menu.py
+++
@@ -1,9 +0,0 @@
-from tnt_nuke import menu, utils, renderman
-
-menu.create() # create tnt menu
-
-import cryptomatte_utilities
-cryptomatte_utilities.setup_cryptomatte_ui()
-
-toolbar = nuke.toolbar("Nodes")
-toolbar.addCommand( "Peregrine/pgBokeh", "nuke.createNode('pgBokeh')", icon="pgBokeh.png")
